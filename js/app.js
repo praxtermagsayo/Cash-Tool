44a70ed93c15c9ef365004b2d24a7eb6597f4054
praxtermagsayo <praxter281407@gmail.com>
2024-05-19 16:10:29 +0800

    added functionality to sign up buttons, and tweaked the font size of the links a bit

--- js/app.js
+++ js/app.js
@@ -1,11 +1,27 @@
-const logInPage = document.getElementById('login-page');
 const landingPage = document.getElementById('home');
+const signUpPage = document.getElementById('signup-page');
+const logInPage = document.getElementById('login-page');
 
 const logInButton = document.getElementById('login');
+const logInLink = document.getElementById('login-link');
+
+const signUpButton = document.getElementById('signup');
+const signUpLink = document.getElementById('register-link');
 
 function toLogInPage(){
     logInPage.style.display = "";
     landingPage.style.display = "none";
+    signUpPage.style.display = "none";
+}
+
+function toSignUpPage(){
+    signUpPage.style.display = "";
+    logInPage.style.display = "none";
+    landingPage.style.display = "none";
 }
 
 logInButton.addEventListener('click', toLogInPage);
+logInLink.addEventListener('click', toLogInPage);
+
+signUpButton.addEventListener('click', toSignUpPage);
+signUpLink.addEventListener('click', toSignUpPage);
