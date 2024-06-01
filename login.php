b9a2c9d919b8d4c214c63019b3201215a89b1768
praxtermagsayo <praxter281407@gmail.com>
2024-06-01 21:41:58 +0800

    fixed the login page and functions

--- login.php
+++ login.php
@@ -18,15 +18,62 @@
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Inter:wght@100..900&display=swap" rel="stylesheet">
     <link rel="icon" href="img/CS.png">
-    <link rel="stylesheet" href="css/index.css">
+    <link rel="stylesheet" href="css/index.css?v=<?php echo time(); ?>">
     <title>Cash Tool</title>
 </head>
 <body>
-
     <section class="page" id="login-page">
         <div id="container">
             <h1 id="header-text">LOGIN</h1>
-            <form action="php/login.php" method="post"  id="form-container">
+
+            <?php
+
+                include('php/connection.php');
+
+                if(isset($_POST['submit'])){
+                    $username = mysqli_real_escape_string($conn, $_POST['username']);
+                    $email = mysqli_real_escape_string($conn, $_POST['email']);
+                    $password = mysqli_real_escape_string($conn, $_POST['password']);
+
+                    $sql = "SELECT * FROM users WHERE username = '$username' AND email = '$email' ";
+                    $result = mysqli_query($conn, $sql);
+
+                    if(mysqli_num_rows($result) == 0){
+                        echo "
+                            <div class = 'message'>
+                                <h3>Account not found, please register first</h3>
+                            </div>
+                        ";
+                        echo "
+                            <a href = 'javascript:self.history.back()'><button id = 'button-submit'>BACK</button></a>
+                        ";
+                    } else {
+                        $user = mysqli_fetch_object($result);
+
+                        if(!password_verify($password, $user->password)){
+                            echo "
+                                <div class = 'message'>
+                                    <h3>Username or password is incorrect</h3>
+                                </div>
+                            ";
+                            echo "
+                                <a href = 'javascript:self.history.back()'><button id = 'button-submit'>BACK</button></a>
+                            ";
+                        } else {
+                            echo "
+                                <script>
+                                    window.location.href = 'home.php'
+                                    alert('Login successful')
+                                </script>
+                            ";
+                        }
+                    }
+
+                } else {
+
+            ?>
+
+            <form action="" method="post"  id="form-container">
                 <input id="login-input" type="text" name="username" placeholder="Username" required>
                 <input id="login-input" type="email" name="email" placeholder="Email" required>
                 <input type="password" id="login-input" name="password" placeholder="Password" required>
@@ -36,7 +83,8 @@
             <p id="register-btn">Don't have an account? <a href="signup.php" id="register-link">Register here</a></p>
         </div>
     </section>
-
+    
+    <?php } ?>
     <section class="page" id="fpass" style="display: none;">
         <div id="container">
             <h1 id="header-text">Forgetful?</h1>
